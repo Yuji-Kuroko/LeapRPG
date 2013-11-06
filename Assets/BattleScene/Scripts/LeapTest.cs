d595cc8332c018eff67b826049fe6d56e600f4ee
HW-Desktop <hogehoge@hoge.com>
2013-11-06 19:17:41 +0900

    Made enviroment of LeapMotion

---
+++ Assets/BattleScene/Scripts/LeapTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LeapTest : MonoBehaviour {
+
+    Controller leapController;
+
+	// Use this for initialization
+	void Start () {
+	    leapController = new Controller();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Frame frame = leapController.Frame();
+
+        Finger finger = frame.Fingers[0];
+        Vector pos = finger.TipPosition;
+        Debug.Log("x = " + pos.x + "   y = " + pos.y + "   z = " + pos.z);
+
+	}
+}
