1a4e63bb6633015a30c0e071aa30bb37e19da4b5
Master <Master@Master-no-MacBook-Air.local>
2013-12-09 19:01:42 +0900

    迷宮移動実装

---
+++ Assets/BattleScene/Scripts/MoverPlayerController.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class MoverPlayerController : MonoBehaviour {
+
+	public Controller leapController = new Controller();
+
+	public static float MOVE_SPEED = 6.0f / 45.0f;
+
+	bool isAct = false;
+
+	enum ActType {
+		MoveForward,
+		MoveBack,
+		TurnRight,
+		TurnLeft,
+		None,
+	}
+
+	ActType nowAct = ActType.None;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//	leap see one hand only.
+		Frame frame = leapController.Frame();
+		if (frame.Hands.Count != 1)
+			return;
+		if (isAct)
+			return;
+
+		Hand hand = frame.Hands[0];
+		if (hand.PalmPosition.z < -10)
+		{
+			nowAct = ActType.MoveForward;
+			StartCoroutine("MoveAction");
+		}
+		else if (hand.PalmPosition.x > 60)
+		{
+			nowAct = ActType.TurnRight;
+			StartCoroutine("MoveAction");
+		}
+		else if (hand.PalmPosition.x < -60)
+		{
+			nowAct = ActType.TurnLeft;
+			StartCoroutine("MoveAction");
+		}
+	}
+
+	IEnumerator MoveAction()
+	{
+		isAct = true;
+		for (int i = 0; i < 45; i++)
+		{
+			switch (nowAct)
+			{
+			case ActType.MoveForward:
+				MoveForward();
+				break;
+			case ActType.MoveBack:
+				MoveBack();
+				break;
+			case ActType.TurnRight:
+				TurnRight();
+				break;
+			case ActType.TurnLeft:
+				TurnLeft();
+				break;
+
+			}
+
+			yield return null;
+		}
+
+
+		isAct = false;
+	}
+
+
+	void TurnRight() {
+		transform.Rotate(new Vector3(0, 2.0f, 0));
+
+	}
+
+	void TurnLeft() {
+		transform.Rotate(new Vector3(0, -2.0f, 0));
+
+	}
+
+	void MoveForward() {
+		//Vector3 pos = transform.position;
+		//transform.position = new Vector3(pos.x, pos.y, pos.z + MOVE_SPEED);
+		transform.Translate(new Vector3(0, 0, MOVE_SPEED));
+	}
+
+	void MoveBack() {
+		transform.Translate(new Vector3(0, 0, MOVE_SPEED));
+	}
+
+}
