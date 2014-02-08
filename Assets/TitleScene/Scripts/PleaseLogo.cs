d2dc4bdd7f63de5bd0f128c9a6084ff78a952b66
Yuji-Kuroko <patchouli.royalflare+git@gmail.com>
2014-02-08 15:30:38 +0900

    タイトルシーンに「手をかざしてください」の文言を追加

---
+++ Assets/TitleScene/Scripts/PleaseLogo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PleaseLogo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		iTween.FadeTo(gameObject, iTween.Hash("alpha", 0.1f, "time", 1.0f, "looptype", iTween.LoopType.pingPong));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+
+
+}
